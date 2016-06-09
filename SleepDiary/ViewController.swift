24e26ea61de3586abfde2f94f2b47c8cd8c15ffc
Katie Smillie <katiesmillie@gmail.com>
2016-06-09 09:56:14 -0400

    Initial commit

---
+++ SleepDiary/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SleepDiary
+//
+//  Created by Katie Smillie on 6/9/16.
+//  Copyright © 2016 Katie Smillie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
