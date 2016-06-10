5c6cc2443fc9f693bdb8b0342ba0fd0dd87ce565
Katie Smillie <katiesmillie@gmail.com>
2016-06-10 08:27:01 -0400

    Date table view cell

---
+++ SleepDiary/DateTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  DateTableViewCell.swift
+//  SleepDiary
+//
+//  Created by Katie Smillie on 6/9/16.
+//  Copyright © 2016 Katie Smillie. All rights reserved.
+//
+
+import UIKit
+
+class DateTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var dateLabel: UILabel?
+    
+    class func cell(tableView: UITableView) -> DateTableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("DateTableViewCell") as! DateTableViewCell
+        return cell
+    }
+    
+    
+    func configure(delegate: DiaryViewModel) {
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .MediumStyle
+        let dateString = formatter.stringFromDate(NSDate())
+        dateLabel?.text = dateString
+    }
+
+}
